72215f0255bd9dceae5f39c5a56fff859b88f46e
GeKaR <radchenko.kotlin.bsx@gmail.com>
2022-08-29 16:29:34 +0300

    added test234

--- app/src/main/java/com/geka/radchenko/yandexwebview/pref/Test.kt
+++ app/src/main/java/com/geka/radchenko/yandexwebview/pref/Test.kt
@@ -2,5 +2,9 @@ package com.geka.radchenko.yandexwebview.pref
 
 data class Test(
     val id: Int,
-    val name:String,
+    val name: String,
+    val name2: String,
+    val name3: String,
+    val name4: String,
+    val name5: String,
 )
